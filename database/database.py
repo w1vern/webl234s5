f8e5918607724d8cf7204b7abe634cde5b27fffc
w1vern <bndvvl@gmail.com>
2024-11-14 23:51:34 +0300

    upd

--- database/database.py
+++ database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 import os
-from database.models.base import Base 
+from database.models import *
 
 import contextlib
 
